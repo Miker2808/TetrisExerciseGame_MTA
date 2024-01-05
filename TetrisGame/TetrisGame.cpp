4fa820d2a27f5a4618f19a4bd12047c6886b747a
miker2808 <miker2808@gmail.com>
2024-01-05 22:48:08 +0200

    change inputHandler

--- TetrisGame/TetrisGame.cpp
+++ TetrisGame/TetrisGame.cpp
@@ -7,29 +7,18 @@ void TetrisGame::play() {
 
     TetrisBoard game_board = TetrisBoard(2, 2); // start board and set it to be x=10,y=10 relative to console
     Tetromino mino = Tetromino(8, 2);
-    unsigned char curr_key = 0;
+    
     unsigned char iter_counter = 0;
     unsigned char drop_counter = 0;
+    unsigned char curr_key = false;
     while (curr_key != 27) { // 27 is "ESC" key
         Sleep(50); // clock
         drop_counter++;
 
         if (_kbhit()) {
             curr_key = _getch();
-
-            if (curr_key == 'a' || curr_key == 'A') 
-                mino.transform(-1, 0, 0);
-            else if (curr_key == 'd' || curr_key == 'D')
-                mino.transform(1, 0, 0);
-            else if (curr_key == 'w' || curr_key == 'W') {
-                mino.transform(0, 0, 1);
-            }
-            else if (curr_key == 's' || curr_key == 'S') {
-                mino.transform(0, 0, -1);
-            }
-            else if (curr_key == 'x' || curr_key == 'X') {
-                mino.transform(0, 1, 0);
-            }
+            // take action depending on user key
+            this->inputHandler(mino, game_board, curr_key);
         }
 
         if (drop_counter > 20){
@@ -59,6 +48,32 @@ bool TetrisGame::checkCollision(Tetromino object, TetrisBoard  board, int posX, 
     }
 
     return true;
+}
+
+// given input character, will move object accordingly, or 
+unsigned char TetrisGame::inputHandler(Tetromino& object, TetrisBoard& board, unsigned char curr_key)
+{
+    if (curr_key == 'a' || curr_key == 'A') {
+        // TODO: check if possible
+        if (true) {
+            object.transform(-1, 0, 0);
+        }
+    }
+    else if (curr_key == 'd' || curr_key == 'D'){
+        object.transform(1, 0, 0);
+    }
+    else if (curr_key == 'w' || curr_key == 'W') {
+        object.transform(0, 0, 1);
+    }
+    else if (curr_key == 's' || curr_key == 'S') {
+        object.transform(0, 0, -1);
+    }
+    else if (curr_key == 'x' || curr_key == 'X') {
+        object.transform(0, 1, 0);
+    }
+
+    return curr_key;
+    
 }
 
 
--- TetrisGame/TetrisGame.h
+++ TetrisGame/TetrisGame.h
@@ -9,4 +9,5 @@ public:
 
 	void play();
 	bool checkCollision(Tetromino object, TetrisBoard board, int posX, int posY);
+	unsigned char inputHandler(Tetromino& object, TetrisBoard& board, unsigned char curr_key);
 };
