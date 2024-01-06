183b6de8eea0b88bca9d861d9da5b9f43331cf0b
Daniel <danielpbea@gmail.com>
2024-01-06 16:10:33 +0200

    Tetromino -  changed the- rot, Xpos , Ypos varubles to private , added 2 methods that return those varubles. changed the check collision func.

--- TetrisGame/TetrisGame.h
+++ TetrisGame/TetrisGame.h
@@ -8,5 +8,5 @@ class TetrisGame {
 public:
 
 	void play();
-	bool checkCollision(Tetromino object, TetrisBoard board, int posX, int posY);
+	bool checkCollision(Tetromino& object, TetrisBoard& board, int move_x, int move_y, int move_rot);
 };
